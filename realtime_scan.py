ee704a1a8ab06a4d9367cdd8a6477ca5522d6260
Matt Fuller <matt@cloudsploit.com>
2017-04-26 16:29:58 -0400

    adding realtime scan example in python

---
+++ realtime_scan.py
@@ -0,0 +1,85 @@
+# This Python script will submit a POST to the
+# realtime scan API endpoint to initiate a new
+# scan. It then polls the endpoint for results.
+
+import sys
+import json
+import time
+import hmac
+import base64
+import hashlib
+import requests
+
+# Obtain a CloudSploit API key and secret from the dashboard
+api_key = "replace-with-key"
+secret = "replace-with-secret"
+key_id = 1234	# The key_id you want to scan (obtain this via GET /keys)
+
+base_url = "https://api.cloudsploit.com"
+interval = 5 # Decreasing this value will result in being rate limited
+
+def make_call(method, path, body):
+	timestamp = str(int(time.time() * 1000))
+	endpoint = base_url + path;
+
+	if body:
+		body_str = json.dumps(body, separators=(',', ':'))
+	else:
+		body_str = ""
+
+	string = timestamp + method + path + body_str
+	signature = hmac.new(secret, msg=string, digestmod=hashlib.sha256).hexdigest()
+
+	hdr = {
+		"Accept": "application/json",
+		"X-API-Key": api_key,
+		"X-Signature": signature,
+		"X-Timestamp": timestamp,
+		"content-type": "application/json"
+	}
+
+	# print method + " " + endpoint
+
+	if method is "POST":
+		r=requests.post(endpoint, headers=hdr, data=body_str);
+	else:
+		r=requests.get(endpoint, headers=hdr);
+
+	return r.text
+
+body = {
+	"key_id": key_id
+}
+
+realtime_scan = make_call("POST", "/v2/realtimes", body)
+
+if realtime_scan is None:
+	print "Error initiating realtime scan: " + realtime_scan
+	sys.exit()
+
+realtime_json = json.loads(realtime_scan)
+
+if "errors" in realtime_json:
+	print(json.dumps(realtime_json, indent=4, separators=(',', ': ')))
+	sys.exit()
+
+realtime_id = realtime_json["data"]["realtime_id"]
+
+if realtime_id is None:
+	print "Error obtaining realtime ID"
+	sys.exit()
+
+print "Scan created with realtime ID: " + str(realtime_id)
+
+while True:
+	print "Polling for results..."
+	realtime_result = make_call("GET", "/v2/realtimes/" + str(realtime_id), None)
+	realtime_result_json = json.loads(realtime_result)
+	realtime_status = realtime_result_json["data"]["status"]
+
+	if realtime_status != "COMPLETE":
+		print realtime_result
+		time.sleep(interval)
+	else:
+		print(json.dumps(realtime_result_json, indent=4, separators=(',', ': ')))
+		sys.exit()
