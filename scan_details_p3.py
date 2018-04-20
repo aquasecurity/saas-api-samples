5741ebd107486589fe0445e96c6064290196ede7
Matt Fuller <matthewdf10@gmail.com>
2018-04-20 08:26:05 -0400

    adding scan detail calls with python3

---
+++ scan_details_p3.py
@@ -0,0 +1,40 @@
+# This Python3 script will query for the details
+# of a scan given a scan ID (with appropriate access)
+
+import json
+import time
+import hmac
+import base64
+import hashlib
+import requests
+
+# Obtain a CloudSploit API key and secret from the dashboard
+api_key = "replace-with-key"
+secret = "replace-with-secret"
+
+# Replace with the ID of the scan
+scan_id = "123"
+
+endpoint = "https://api.cloudsploit.com"
+path = "/v2/scans/" + scan_id
+method = "GET"
+timestamp = str(int(time.time() * 1000))
+
+string = timestamp + method + path
+
+secret_bytes= bytes(secret , 'latin-1')
+string_bytes = bytes(string, 'latin-1')
+
+signature = hmac.new(secret_bytes, msg=string_bytes, digestmod=hashlib.sha256).hexdigest()
+
+hdr = {
+	"Accept": "application/json",
+	"X-API-Key": api_key,
+	"X-Signature": signature,
+	"X-Timestamp": timestamp,
+	"content-type": "application/json"
+}
+
+r=requests.get(endpoint + path, headers=hdr);
+
+print(r.text)
---
+++ scan_list_p3_params.py
@@ -0,0 +1,44 @@
+# This Python3 script will query for a list of scans
+# including limit and offset values of a scan
+
+import json
+import time
+import hmac
+import base64
+import hashlib
+import requests
+
+# Obtain a CloudSploit API key and secret from the dashboard
+api_key = "replace-with-key"
+secret = "replace-with-secret"
+
+# Include limit and offset here
+# Set the limit=1 and remove "offset" entirely (don't set to 0)
+# to retrieve the latest scan
+limit="1"
+offset="5"
+
+endpoint = "https://api.cloudsploit.com"
+path = "/v2/scans"
+args = "?limit=" + limit + "&offset=" + offset
+method = "GET"
+timestamp = str(int(time.time() * 1000))
+
+string = timestamp + method + path
+
+secret_bytes= bytes(secret , 'latin-1')
+string_bytes = bytes(string, 'latin-1')
+
+signature = hmac.new(secret_bytes, msg=string_bytes, digestmod=hashlib.sha256).hexdigest()
+
+hdr = {
+	"Accept": "application/json",
+	"X-API-Key": api_key,
+	"X-Signature": signature,
+	"X-Timestamp": timestamp,
+	"content-type": "application/json"
+}
+
+r=requests.get(endpoint + path + args, headers=hdr);
+
+print(r.text)
